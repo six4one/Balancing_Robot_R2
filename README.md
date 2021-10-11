4b197c9a7c4360e4e4b864158725e0847ba980dc
Fausto Zecca <fzecca@alumicast.ca>
2021-10-11 10:05:30 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://six4one.github.io/balance_x13/](https://six4one.github.io/balance_x13/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/six4one/balance_x13** and import
+
+## Edit this project ![Build status badge](https://github.com/six4one/balance_x13/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/six4one/balance_x13** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/six4one/balance_x13/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,184 @@
+
+let offSets: number[] = [-77, -25, 44, 145, 125, 62]; // MPU-6050
+let selfCal: boolean = microbit_GY521.calibrate_Sensors(offSets);
+// required to increase accuracy of readings
+//
+if (!selfCal) {
+    basic.showLeds(`
+        # . . # .
+        # . # # #
+        # . . # .
+        # . # # #
+        # . . # .
+        `)
+    basic.pause(30000);
+}
+
+
+// Motor state setup
+// Use to trim motors if one is stronger than the other
+let leftMotorTrim = 0;
+let rightMotorTrim = 0;
+let steeringBias: number = 0;
+let maxSteerBias: number = 50; //Maximum turn rate
+let outMax = 1023;
+let outMin = -1023;
+
+//**************************
+//****PID state setup*******
+//**************************
+let Kp = 120;  //90
+let Ki = 2000;  //900 
+let Kd = 1.9;  //1.5
+// Manage baseline motor speed ie the value from which motor values have an affect
+
+let motorMin = 30;  //138
+let blackOut: boolean = false;
+let setPoint = 0;
+let y_Offset: number = -98.5;
+
+let cycleDelay = 2;  //miliseconds
+let y: number = 0;
+let yTrim: number = 0;
+let speed: number = 0;
+let lean: number = 0;
+let leanLimit: number = 0.5;   //Maximum controlled lean allowed in degrees(travel speed)
+let lastInput = 0;
+let lastTime = 0;
+let accumulative_error: number = 0;
+let last_error: number = 0;
+let Actuator_Output: number = 0;
+let gap: number = 0;  //Absolute difference between setpoint and y
+
+let iClamp: boolean = false  //dissable the integrator function when actuator is saturated
+
+led.plotBarGraph(1000, 1023);
+basic.pause(500);
+led.plotBarGraph(750, 1023);
+basic.pause(500);
+led.plotBarGraph(500, 1023);
+basic.pause(500);
+led.plotBarGraph(250, 1023)
+basic.pause(500);
+
+basic.showLeds(`
+    . # . . .
+    # . . # .
+    # . . . .
+    # . . # .
+    . # . . .
+    `)
+
+input.onButtonPressed(Button.A, () => {
+    yTrim -= 0.25;
+});
+
+input.onButtonPressed(Button.B, () => {
+    yTrim += 0.25;
+});
+
+let i = 0
+while (i < 1000) {  //discard the first set of data from the MPU6050 to allow proper initialization
+    let discard: number = 0
+    discard = microbit_GY521.computeY()
+    basic.pause(2)
+    i++
+}
+led.enable(false)
+while (1) {
+    basic.pause(cycleDelay);  //Miliseconds
+    y = microbit_GY521.computeY() + y_Offset + yTrim;
+
+
+    gap = Math.abs(setPoint - y);
+
+    if (gap > 25) {
+        blackOut = true;
+    }
+    if (gap < 2) {
+        blackOut = false;
+    }
+
+    if (blackOut) {
+        motorStop();
+    }
+    else {
+
+        speed = PID(-y);
+        motorController(speed);
+
+    }
+}
+
+function PID(input: number): number {
+    /*
+        PID algorithm
+            Actuator_Output =
+                Kp * (distance from goal) 
+            +   Ki * (accumulative error)
+            +   Kd * (change in error)
+    */
+    let now = game.currentTime();    // in milliseconds
+    let timeChange = (now - lastTime) / 1000;    //convert to seconds
+    let distance_from_goal: number = (setPoint - input); // P
+    if (iClamp) {
+        accumulative_error = 0; // I (Clamped State)
+    } else {
+        accumulative_error = accumulative_error + (distance_from_goal * timeChange); // I (Normal State)
+    }
+    let change_in_error = (distance_from_goal - last_error) / timeChange; // D
+    last_error = distance_from_goal;
+
+    Actuator_Output = Kp * distance_from_goal + Ki * accumulative_error + Kd * change_in_error;
+
+    let outputAbs: number = Math.abs(Actuator_Output) + motorMin;    // A base motor speed to over come motor inertia and restrict maximum power 
+    if (outputAbs > outMax) {  //Saturation Check
+        (outputAbs = outMax);
+        if (Math.sign(distance_from_goal) == (Math.sign(Actuator_Output))) {
+            //if ((distance_from_goal * Actuator_Output) > 0) {
+            iClamp = true;
+        }
+    } else {
+        iClamp = false;
+    }
+    if (Actuator_Output < 0) {
+        Actuator_Output = -outputAbs;
+    } else {
+        Actuator_Output = outputAbs;
+    }
+    lastInput = input;
+    lastTime = now;
+
+    return Actuator_Output;
+}
+
+function motorStop() {
+    pins.digitalWritePin(DigitalPin.P13, 0);
+    pins.digitalWritePin(DigitalPin.P14, 0);
+    pins.digitalWritePin(DigitalPin.P15, 0);
+    pins.digitalWritePin(DigitalPin.P16, 0);
+}
+
+function motorController(speed: number) {
+
+    if (speed > 0) {
+        pins.digitalWritePin(DigitalPin.P13, 1);
+        pins.digitalWritePin(DigitalPin.P14, 0);
+        pins.digitalWritePin(DigitalPin.P15, 1);
+        pins.digitalWritePin(DigitalPin.P16, 0);
+
+    }
+    if (speed < 0) {
+
+        pins.digitalWritePin(DigitalPin.P13, 0);
+        pins.digitalWritePin(DigitalPin.P14, 1);
+        pins.digitalWritePin(DigitalPin.P15, 0);
+        pins.digitalWritePin(DigitalPin.P16, 1);
+    }
+
+    pins.analogWritePin(AnalogPin.P0, Math.abs(speed + rightMotorTrim + steeringBias));
+    pins.analogSetPeriod(AnalogPin.P0, 2500);
+    pins.analogWritePin(AnalogPin.P1, Math.abs(speed + leftMotorTrim - steeringBias));
+    pins.analogSetPeriod(AnalogPin.P1, 2500);
+
+}
